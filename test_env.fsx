56db33ed2a522d42c385aea922ba89329a5547ff
Kevin Schneider <schneider.kev@outlook.de>
2024-03-19 13:44:19 +0100

    another one

--- test_env.fsx
+++ test_env.fsx
@@ -4,3 +4,6 @@ open System.Text
 
 let GITHUB_ENV = Environment.GetEnvironmentVariable("GITHUB_ENV")
 File.AppendAllLines(GITHUB_ENV, [ "SCRIPT_OUTPUT=lol" ])
+
+printfn "env : %s" GITHUB_ENV
+printfn $"vals : {File.ReadAllText(GITHUB_ENV)}" 
